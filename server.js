ca3dcfb92ed0217422deab3f97f08a2a27a0a88b
Megaemce <1651451+Megaemce@users.noreply.github.com>
2024-06-21 20:53:52 +0700

    update to fix stupid uppercase issue

--- server.js
+++ server.js
@@ -11,13 +11,16 @@ app.use(express.static(path.join(__dirname, "_site")));
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+// Basic health check
+app.get("/api/", (req, res) => res.json({ message: "Hello from Express!" }));
+
 // Get the likes count
 app.get("/api/:postTitle/likes", async (req, res) => {
     try {
         const { postTitle } = req.params;
         const likes =
-            (await kv.HGET(postTitle, "likes")) ||
-            (await kv.HSET(postTitle, "likes", 0));
+            (await kv.hget(postTitle, "likes")) ||
+            (await kv.hget(postTitle, "likes", 0));
 
         res.json({ likes: likes });
     } catch (err) {
@@ -30,7 +33,7 @@ app.get("/api/:postTitle/likes", async (req, res) => {
 app.post("/api/:postTitle/likes", async (req, res) => {
     try {
         const { postTitle } = req.params;
-        const likes = await kv.HINCRBY(postTitle, "likes", 1);
+        const likes = await kv.hincrby(postTitle, "likes", 1);
 
         res.json({ likes: likes });
     } catch (err) {
@@ -43,13 +46,18 @@ app.post("/api/:postTitle/likes", async (req, res) => {
 app.get("/api/:postTitle/views", async (req, res) => {
     try {
         const { postTitle } = req.params;
-        const views = await kv.HINCRBY(postTitle, "views", 1);
+        const views = await kv.hincrby(postTitle, "views", 1);
 
         res.json({ views: views });
     } catch (err) {
         console.error(err);
         res.status(500).send("Error getting post views!");
     }
+});
+
+// Catch-all handler to serve the index.html for any request that doesn't match an API route
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "_site", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
