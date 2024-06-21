8e60f0c6aeba1cb1e4cc2a486e5f231267baec13
Megaemce <1651451+Megaemce@users.noreply.github.com>
2024-06-21 20:09:59 +0700

    add button and view, and proper api

--- server.js
+++ server.js
@@ -1,23 +1,59 @@
-// server.js
 const express = require("express");
-const path = require("path");
+const kv = require("@vercel/kv");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-// Serve static files from the _site directory (the default output directory of 11ty)
-app.use(express.static(path.join(__dirname, "_site")));
+let likes = 0;
+let views = 0;
 
-// Example of a dynamic API route
-app.get("/api/*", (req, res) => {
-    res.json({ message: "Hello from Express!" });
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Basic health check
+app.get("api/", (req, res) => res.send("Express on Vercel"));
+
+// Get the likes count
+app.get("api/:postTitle/likes", async (req, res) => {
+    try {
+        const { postTitle } = req.params;
+        likes =
+            (await kv.HGET(postTitle, "likes")) ||
+            (await kv.HSET(postTitle, "likes", 0));
+
+        res.json({ likes: likes });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error getting likes count!");
+    }
 });
 
-// Catch-all handler to serve the index.html for any request that doesn't match an API route
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "_site", "index.html"));
+// Update the likes count
+app.post("api/:postTitle/likes", async (req, res) => {
+    try {
+        const { postTitle } = req.params;
+        likes = await kv.HINCRBY(postTitle, "likes", 1);
+
+        res.json({ likes: likes });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error updating likes count!");
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Get the view and increment them. Every request is a new view
+app.get("api/:postTitle/views", async (req, res) => {
+    try {
+        const { postTitle } = req.params;
+        views = await kv.HINCRBY(postTitle, "views", 1);
+
+        res.json({ views: views });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error getting post views!");
+    }
 });
+
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+
+module.exports = app;
